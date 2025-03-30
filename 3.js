24504e5b3e89990733db7c4c2edad2848ca1cbe0
Rishabh07code <rishabhgdsc100@gmail.com>
2025-03-30 12:18:18 +0530

    page 1 done

--- 3.js
+++ 3.js
@@ -1,4 +1,5 @@
-
+function loadingAnimation(){
+    
 var tl=gsap.timeline()
 tl.from(".line h1",{ 
     y:150,
@@ -27,16 +28,39 @@ tl.from(".line h1",{
 tl.to("#loader",{
     opacity:0,
     duration:0.2,
-    delay:4
+    delay:0
 })
 tl.from("#page1",{
     opacity:0,
     delay:0.2,
-    y:1600,
-    ease:Power4
+    y:1200,
+    ease:Power4,
+    duration:0.6
 
 })
 
 tl.to("#loader",{
-    display:"none"
+    display:"none",
+});
+tl.from("#nav",{
+    opacity:0
 })
+tl.from("#hero1 h1,#hero2 h1,#hero3 h2,#hero4 h1",{
+    y:120,
+    stagger:0.2
+})
+
+}
+loadingAnimation()
+
+function cursorAnimation(){
+    document.addEventListener("mousemove",function(dets){
+        gsap.to("#crsr",{
+        left:dets.x,
+        top:dets.y
+        })
+    })
+    Shery.makeMagnet("#nav-part2 h5");
+}
+
+cursorAnimation()
